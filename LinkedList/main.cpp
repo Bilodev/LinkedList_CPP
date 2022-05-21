9e18350878b2c3dee4e661de05d4d1ce3f5804fd
Antonio Bilotta <104415158+Bilodev@users.noreply.github.com>
2022-05-21 23:32:12 +0200

    New Debug

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -3,14 +3,9 @@
 
 int main()
 {
-    LinkedList HEAD({1,2,3,4,5});
-    std::cout << HEAD[4];
-    std::cout << HEAD[0];
-
-
-
-    
+    typedef signed long long int euro;
+    LinkedList<euro> Money({1,2,3,4,5,6,7,8});
+    Money.pop();
+    Money.display();
     return 0;
-    
-
 }
